45b8ffc2b8606d819c930ff7a400e58ddea05137
fervi <thunderstormpolska@o2.pl>
2017-04-20 18:47:24 +0200

    Add telnet support (for local login)

--- build-replinux.sh
+++ build-replinux.sh
@@ -6,5 +6,9 @@ echo "deb-src http://ports.ubuntu.com/ xenial main universe" >> /etc/apt/sources
 apt-get update
 apt-mark hold libc6
 apt-get -y dist-upgrade
+
+#Telnet support (for login without SSH in app)
+apt-get -y install telnetd
+
 apt-get -y autoremove
 apt-get clean
